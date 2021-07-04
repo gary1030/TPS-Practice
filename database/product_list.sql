60c3337ae3552b5b6569d96d5a6c592299142c4d
guanyiiiii-0225 <zoememy@gmail.com>
2021-07-04 23:13:47 +0800

    Database

---
+++ database/product_list.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `product` (
+  `product_id` int(3) NOT NULL,
+  `product_name` char(10) DEFAULT NULL
+)
+
+INSERT INTO `product` (`product_id`, `product_name`) VALUES
+(1, 'THF'),
+(2, 'BQD'),
+(3, 'PRN'),
+(4, 'PLL'),
+(5, 'XEC'),
+(6, 'YNS'),
+(7, 'BBB'),
+(8, 'CII'),
+(9, 'SPV'),
+(10, 'LRC'),
+(11, 'HDV'),
+(12, 'ULT'),
+(13, 'PQJ'),
+(14, 'YQL'),
+(15, 'GVM'),
+(16, 'VSF'),
+(17, 'SCH'),
+(18, 'FDX'),
+(19, 'HQJ'),
+(20, 'VDD');
