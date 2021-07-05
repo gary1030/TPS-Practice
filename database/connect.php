9a6e77b30b7cc032028598960da804fc56cc019b
Shannon1923 <86880073+Shannon1923@users.noreply.github.com>
2021-07-05 23:33:41 +0800

    Update connect.php

--- database/connect.php
+++ database/connect.php
@@ -19,28 +19,6 @@ $success = mysqli_real_connect(
 // UTF8
 mysqli_set_charset($link, 'utf8');
 
-// $sql = "SELECT * FROM member";
-// if($stmt = $link -> query($sql))
-// {
-//     /*
-//     while($result = mysqli_fetch_object($stmt))
-//     {
-//         echo '<p> ID: '.$result -> member_id.' </p> ';
-//     }*/
-//     echo " <table width='500' height='120' border='1'>";
-//     while($row = mysqli_fetch_array($stmt)) 
-//     { 
-//         echo "<tr>";
-//         for ($j=0; $j<5; $j++)
-//         { //每行有 5 個欄位
-
-//             echo "<td height='30'>$row[$j]</td>";   // 欄位高 30 pix
-
-//         }
-//         echo "</tr>";
-//     } 
-// }
-
 // function one
 function memberTransDetail($ID, $link){
     $sql = "SELECT *
@@ -94,7 +72,136 @@ function memberTransDetail($ID, $link){
 }
 memberTransDetail(3, $link);
 
+// fuction 2
+function productTransDetail($ID, $link){
+    /*function two (input product id and get all the transaction record)*/
+    $sql = "SELECT transaction_id, transaction_date, transaction.member_id, transaction_price
+    FROM transaction
+    WHERE transaction.product_id = $ID";
 
+
+    if($stmt = $link -> query($sql))
+    {
+        echo " <table width='500' height='120' border='1'>";
+        echo " <tr height='50' align='center'>";
+        echo " <td width = '50'> 交易ID </td>";
+        echo " <td width = '80'> 交易日期 </td>";
+        echo " <td width= '50'> 會員ID </td>";
+        echo " <td width = '80'> 交易金額 </td>";
+        //echo " <td width = '160'> member_date </td>";
+        echo "</tr>";
+        while($row = mysqli_fetch_array($stmt)) 
+        { 
+            echo "<tr align='center'>";
+            for ($j=0; $j<4; $j++)
+            { //每行有 4 個欄位
+                echo "<td height='30'>$row[$j]</td>";   // 欄位高 30 pix
+            }
+            echo "</tr>";
+        } 
+    }
+
+    /*function two (input product id and get transaction sum & times from each member)*/
+    $sql = "SELECT transaction.member_id, COUNT(transaction.member_id), SUM(transaction.transaction_price)
+    FROM transaction
+    WHERE product_id = $ID
+    GROUP BY member_id
+    ORDER BY member_id";
+
+
+    if($stmt = $link -> query($sql))
+    {
+        echo " <table width='500' height='120' border='1'>";
+        echo " <tr height='50' align='center'>";
+        echo " <td width = '50'> 會員ID </td>";
+        echo " <td width = '50'> 歷史購買次數 </td>";
+        echo " <td width= '80'> 購買累計金額 </td>";
+        //echo " <td width = '160'> 交易金額 </td>";
+        //echo " <td width = '160'> member_date </td>";
+        echo "</tr>";
+        while($row = mysqli_fetch_array($stmt)) 
+        { 
+            echo "<tr align='center'>";
+            for ($j=0; $j<3; $j++)
+            { //每行有 3 個欄位
+                echo "<td height='30'>$row[$j]</td>";   // 欄位高 30 pix
+            }
+            echo "</tr>";
+        } 
+    }
+}
+productTransDetail(1, $link);
+
+
+
+// function 4
+//option = TRUE for gender, FALSE for age level.
+function summaryTrans($option, $ID, $link){
+        
+    if($option)
+    {
+        /*function four (input product id and output transaction sum & times for different genders)*/
+        $sql = "SELECT member.member_gender, COUNT(transaction.member_id), SUM(transaction.transaction_price)
+        FROM member
+        JOIN transaction
+        ON member.member_id = transaction.member_id
+        WHERE transaction.product_id = $ID
+        GROUP BY member.member_gender
+        ORDER BY member.member_gender";
+
+        if($stmt = $link -> query($sql))
+        {
+            echo " <table width='500' height='120' border='1'>";
+            echo " <tr height='50' align='center'>";
+            echo " <td width = '50'> 會員性別 </td>";
+            echo " <td width = '50'> 消費總人次 </td>";
+            echo " <td width= '80'> 消費總金額 </td>";
+            echo "</tr>";
+            while($row = mysqli_fetch_array($stmt)) 
+            { 
+                echo "<tr align='center'>";
+                for ($j=0; $j<3; $j++)
+                { //每行有 3 個欄位
+                    echo "<td height='30'>$row[$j]</td>";   // 欄位高 30 pix
+                }
+                echo "</tr>";
+            } 
+        }
+    }
+    else
+    {
+        /*function four (input product id and output transaction sum & times for different age level)
+        */
+        /*Where age level is 0 for age group below 20, 1 for age group 20-30, 2 for age group 30-40, 3 for age group 40 above.*/
+        $sql = "SELECT INTERVAL(DATEDIFF('2022-01-01', member.member_BD),7305,10958,14610) AS ageLevel, COUNT(transaction.member_id), SUM(transaction.transaction_price)
+        FROM member
+        JOIN transaction
+        ON member.member_id = transaction.member_id
+        WHERE transaction.product_id = $ID
+        GROUP BY ageLevel
+        ORDER BY ageLevel";
+
+        if($stmt = $link -> query($sql))
+        {
+            echo " <table width='500' height='120' border='1'>";
+            echo " <tr height='50' align='center'>";
+            echo " <td width = '50'> 年齡區間 </td>";
+            echo " <td width = '50'> 消費總人次 </td>";
+            echo " <td width= '80'> 消費總金額 </td>";
+            echo "</tr>";
+            while($row = mysqli_fetch_array($stmt)) 
+            { 
+                echo "<tr align='center'>";
+                for ($j=0; $j<3; $j++)
+                { //每行有 3 個欄位
+                    echo "<td height='30'>$row[$j]</td>";   // 欄位高 30 pix
+                }
+                echo "</tr>";
+            } 
+        }
+    }
+}
+summaryTrans(FALSE, 1, $link);//option = TRUE for gender, FALSE for age level.
 
 
 ?>
