9037a1a377770e07034ac3e88c796f0f59e58d84
Brian-Konr <b08705038@ntu.edu.tw>
2021-07-05 22:29:05 +0800

    Update LookUpClient.vue

--- frontend/src/views/LookUpClient.vue
+++ frontend/src/views/LookUpClient.vue
@@ -1,8 +1,58 @@
 <template>
   <div>
     <client-form-by-id @look-up-member="searchMember"></client-form-by-id>
-    <client-form-by-list @look-up-member="searchMember"></client-form-by-list>
+    <client-form-by-list 
+      @look-up-member="searchMember"
+      v-bind:members="members"
+    ></client-form-by-list>
   </div>
+  <br/>
+  <!-- <h3>All Transaction Records</h3>
+  <div id='container'>
+    <table>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>trans date</th>
+        <th>product</th>
+        <th>buyer</th>
+        <th>price</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(tran, index) in trans" :key="index">
+        <td>{{index + 1}}</td>
+        <td>{{tran.transaction_date}}</td>
+        <td>{{tran.product_id}}</td>
+        <td>{{tran.member_id}}</td>
+        <td>{{tran.transaction_price}}</td>
+      </tr>
+    </tbody>
+    </table>
+  </div> -->
+  <!-- <br/> -->
+  <!-- <div>Total spent: {{total}}</div> -->
+  <h3>{{cur_member.name}}'s Transaction Records</h3>
+  <div id='container'>
+    <table>
+      <thead>
+        <tr>
+          <th>product</th>
+          <th>trans times</th>
+          <th>trans amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(product, index) in record" :key="index">
+          <td>{{product.name}}</td>
+          <td>{{product.trans_cnt}}</td>
+          <td>{{product.trans_amount}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <h3>{{cur_member.name}}'s total spent$: {{this.cur_total}}</h3>
+  <br/>
 </template>
 
 <script>
@@ -16,75 +66,82 @@ export default {
   },
   data() {
     return {
-      trans: [
-        {
-          transaction_id: 1,
-          transaction_date: 20210704,
-          member_id: 1,
-          product_id: 2,
-          transaction_price: 100,
-        },
-        {
-          transaction_id: 2,
-          transaction_date: 20210705,
-          member_id: 2,
-          product_id: 2,
-          transaction_price: 200,
-        },
-        {
-          transaction_id: 3,
-          transaction_date: 20210706,
-          member_id: 1,
-          product_id: 1,
-          transaction_price: 300,
-        },
-        {
-          transaction_id: 4,
-          transaction_date: 20210707,
-          member_id: 3,
-          product_id: 2,
-          transaction_price: 500,
-        },
-        {
-          transaction_id: 5,
-          transaction_date: 20210708,
-          member_id: 1,
-          product_id: 3,
-          transaction_price: 100,
-        },
-        {
-          transaction_id: 6,
-          transaction_date: 20210709,
-          member_id: 5,
-          product_id: 3,
-          transaction_price: 200,
-        },
-        {
-          transaction_id: 7,
-          transaction_date: 20210710,
-          member_id: 1,
-          product_id: 1,
-          transaction_price: 300,
-        },
-        {
-          transaction_id: 8,
-          transaction_date: 20210711,
-          member_id: 1,
-          product_id: 2,
-          transaction_price: 100,
-        },
-      ],
+      trans: [],
       members: [],
       products: [],
       cur_member: { id: 1, name: "Ann" },
+      cur_trans: [],
+      record: [],
+      cur_total: ''
     };
   },
   methods: {
-    searchMember(memberId) {
+    async searchMember(memberId) {
       console.log("searching: ", memberId);
+      this.cur_member.id = memberId;
+      this.cur_member.name = this.members.find(
+        (ele) => ele.id === memberId
+      ).name;
+      
+      var id = this.cur_member.id;
+
+      // get transaction data
+      try {
+        const res = await axios.get(`http://localhost:3000/transactions`);
+        this.trans = res.data;
+      } catch(e) {
+        console.error(e);
+      }
+
+      // cur_tran means we only care about trans of current chosen member
+      var cur_tran = this.trans.filter(function(item){
+        return item.member_id === id;
+      });
+      console.log(cur_tran);
+      this.cur_trans = cur_tran;
+
+
+      var product_trans = this.products;
+      product_trans.forEach((e) => {
+        e.trans_cnt = 0;
+        e.trans_amount = 0;
+      });
+      
+      //temp is used for calculating current member's total spent
+      var temp = 0;
+      cur_tran.forEach((tran) => {
+        var index = product_trans.findIndex((ele) => ele.id === tran.product_id)
+        if (index !== -1) {
+          product_trans[index].trans_cnt += 1;
+          product_trans[index].trans_amount += tran.transaction_price;
+          temp = product_trans[index].trans_amount;
+        }
+      });
+      this.record = product_trans;
+      this.cur_total = temp;
+
     },
   },
+  computed: {
+    total: function(){
+      console.log(this.trans);
+      return this.trans.reduce(function(total, tran){
+        return total + tran.transaction_price;
+      }, 0);
+    },
+    // curTotal: function(){
+    //   return this.cur_trans.reduce(function(total, tran){
+    //     return total + tran.trans_amount;
+    //   })
+    // }
+  },
   async created() {
+    try {
+      const res = await axios.get(`http://localhost:3000/transactions`);
+      this.trans = res.data;
+    } catch(e) {
+      console.error(e);
+    }
     try {
       const res = await axios.get(`http://localhost:3000/members`);
 
@@ -103,4 +160,10 @@ export default {
   },
 };
 </script>
+
+<style type="text/css">
+#container {
+  margin: 0 auto; width: 15%;
+}
+</style>
 
