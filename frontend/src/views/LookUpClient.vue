3c4c1b9884ee9dafb59ec8c18f39aa46cc53c875
gary1030 <gary6658@ntu.im>
2021-07-07 19:48:25 +0800

    noice

--- frontend/src/views/LookUpClient.vue
+++ frontend/src/views/LookUpClient.vue
@@ -1,31 +1,34 @@
 <template>
   <div>
     <client-form-by-id @look-up-member="searchMember"></client-form-by-id>
-    <client-form-by-list 
+    <client-form-by-list
       @look-up-member="searchMember"
       v-bind:members="members"
     ></client-form-by-list>
-  </div>
-  <br/>
-  <h3 v-if="isSearched">{{cur_member.member_name}}的每日平均花費為: {{this.cur_member_total[0].consumption_per_day}}</h3>
-  <h3 v-if="isSearched">{{cur_member.member_name}} 的歷史消費紀錄</h3>
-  <table-lite
+    <br />
+    <h3 v-if="isSearched">
+      {{ cur_member.member_name }}的每日平均花費為:
+      {{ this.cur_member_total[0].consumption_per_day }}
+    </h3>
+    <h3 v-if="isSearched">{{ cur_member.member_name }} 的歷史消費紀錄</h3>
+    <table-lite
       :columns="table2.columns"
       :rows="table2.rows"
       :total="table2.record"
       :sortable="table2.sortable"
       :messages="table2.messages"
       @do-search="doSearch"
-  ></table-lite>
-  <h3 v-if="isSearched">{{ cur_member.member_name }} 的交易列表</h3>
-  <table-lite
+    ></table-lite>
+    <h3 v-if="isSearched">{{ cur_member.member_name }} 的交易列表</h3>
+    <table-lite
       :columns="table.columns"
       :rows="table.rows"
       :total="table.record"
       :sortable="table.sortable"
       :messages="table.messages"
       @do-search="doSearch"
-  ></table-lite>
+    ></table-lite>
+  </div>
 </template>
 
 <script>
@@ -46,7 +49,7 @@ export default {
       members: [],
       products: [],
       isSearched: false,
-      cur_member: {member_id: "", member_name: ""},
+      cur_member: { member_id: "", member_name: "" },
       memberProTrans: [],
       memberAllTrans: [],
       cur_member_total: [],
@@ -72,10 +75,10 @@ export default {
             field: "transAmount",
             width: "10%",
             sortable: false,
-          }
+          },
         ],
         rows: [],
-        sortable:{
+        sortable: {
           order: "id",
           sort: "asc",
         },
@@ -85,7 +88,7 @@ export default {
           gotoPageLabel: "Go to page:",
           noDataAvailable: "No data",
         },
-        record: 0
+        record: 0,
       },
       table2: {
         isLoading: false,
@@ -114,10 +117,10 @@ export default {
             field: "transaction_price",
             width: "10%",
             sortable: false,
-          }
+          },
         ],
         rows: [],
-        sortable:{
+        sortable: {
           order: "id",
           sort: "asc",
         },
@@ -128,13 +131,13 @@ export default {
           noDataAvailable: "No data",
         },
         record: 0,
-      }
+      },
     };
   },
   methods: {
     async searchMember(memberId) {
       var exist = this.members.find((ele) => ele.member_id === memberId);
-      if (!exist){
+      if (!exist) {
         alert("ID not Exists!");
         return;
       }
@@ -166,8 +169,10 @@ export default {
       } catch (e) {
         console.error(e);
       }
-      this.memberProTrans.forEach((e) =>{
-        e.product_name = this.products.find((ele) => ele.product_id === e.product_id).product_name;
+      this.memberProTrans.forEach((e) => {
+        e.product_name = this.products.find(
+          (ele) => ele.product_id === e.product_id
+        ).product_name;
       });
       this.table.rows = this.memberProTrans;
 
@@ -178,21 +183,22 @@ export default {
         });
         this.memberAllTrans = res.data;
         this.table2.record = res.data.length;
-        
       } catch (e) {
         console.error(e);
       }
-      this.memberAllTrans.forEach((e) =>{
-        e.product_name = this.products.find((ele) => ele.product_id === e.product_id).product_name;
+      this.memberAllTrans.forEach((e) => {
+        e.product_name = this.products.find(
+          (ele) => ele.product_id === e.product_id
+        ).product_name;
       });
       this.table2.rows = this.memberAllTrans;
-      
+
       try {
         const res = await axios.post("http://localhost:8888/connect.php", {
           action: "getMemberAvgCost",
           params: memberId,
         });
-        
+
         this.cur_member_total = res.data;
       } catch (e) {
         console.error(e);
@@ -214,8 +220,8 @@ export default {
     },
   },
   computed: {
-    total: function(){
-      return this.trans.reduce(function(total, tran){
+    total: function () {
+      return this.trans.reduce(function (total, tran) {
         return total + tran.transaction_price;
       }, 0);
     },
@@ -236,7 +242,8 @@ export default {
 
 <style type="text/css">
 #container {
-  margin: 0 auto; width: 15%;
+  margin: 0 auto;
+  width: 15%;
 }
 </style>
 
